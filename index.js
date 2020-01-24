549893f56d57089142f9245e9d1673d876f7b138
Dal Ponte <maico.dalponte@codengage.com>
2020-01-24 03:31:15 -0300

    feat: Add GPIO lib to the service functions

--- index.js
+++ index.js
@@ -1,48 +1,45 @@
 const { fromEvent } = require('rxjs');
 const readline = require('readline')
 const EventEmitter = require('events')
-const rpi = require('./rpiService')
+const rpiService = require('./rpiService')
 
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true)
-console.info('Press CTRL + C to end program')
 const keypress = fromEvent(process.stdin, 'keypress')
 
-const mock = new EventEmitter();
-// Only for visual feedback
-mock.on('change', (e) => console.log(e));
 const genericCallBack = (r) => console.warn(' == Callback == ', r)
 
-keypress.subscribe(([event, key]) => {
-    switch (event) {
+rpiService.init()
+    .then(rpi => {
+        console.info('Press CTRL + C to end program')
 
-        case '\r':
-            // Emit node event to simulate IO with delay
-            mock.emit('change', { value: true })
-            break
+        keypress.subscribe(([event, key]) => {
+            switch (event) {
+                case '1':  // Manualy open the valve
+                    rpiService.openValve(rpi, genericCallBack)
+                    break
 
-        case '1':
-            rpi.openValve( genericCallBack)
-            break
+                case '2': // Manualy close the valve
+                    rpiService.closeValve(rpi, genericCallBack)
+                    break
 
-        case '2':
-            rpi.closeValve(genericCallBack)
-            break
+                case ' ': // Start to listen events and acumulate
 
-        case 's':
-            // Start to listen events and acumulate
-            rpi.openValve( () => console.log('open'))
-            const flow = rpi.listenChannel({target: mock})
-            flow.subscribe(({channel, pulses }) => {
-                console.warn(channel, pulses)
-                if (pulses >= 10) {
-                    rpi.closeValve( () => console.log('close'))
-                }
-            })
-            break
+                    rpiService.openValve(rpi, () => genericCallBack)
+                    const flow = rpiService.listenChannel(rpi)
+                    flow.subscribe(({ pulses, counter}) => {
+                        console.warn('Flux:', pulses, counter)
+                        if (pulses >= 1000) {
+                            rpiService.closeValve(rpi, () => process.exit())
+                        }
+                    })
+                    break
 
-        case '\u0003': process.exit()
-        default: console.log(JSON.stringify(key))
-    }
-});
+                case '\u0003': process.exit()
+                default: console.log(JSON.stringify(key))
+            }
+        });
 
+
+    })
+    .catch(err => console.error(err))
--- rpiService.js
+++ rpiService.js
@@ -1,27 +1,48 @@
 const { fromEvent } = require('rxjs');
 const { scan } = require('rxjs/operators');
+var gpio = require('rpi-gpio');
+
 
 VALVE_CHANNEL = 11
 FLUX_CHANNEL = 13
 
-const openValve = (callBack) => {
-    return callBack({ channel: VALVE_CHANNEL })
+const openValve = (target, callBack) => {
+    target.write(11, false, function (err) {
+        if (err) throw err;
+        callBack({ channel: VALVE_CHANNEL, action: 'open' })
+    });
 }
 
-const closeValve = (callBack) => {
-    return callBack({ channel: VALVE_CHANNEL })
+const closeValve = (target, callBack) => {
+    target.write(11, true, function (err) {
+        if (err) throw err;
+        callBack({ channel: VALVE_CHANNEL, action: 'close' })
+    });
 }
 
-const listenChannel = ({ target }) => {
+const listenChannel = (target) => {
+    let counter = 0
     const listener = fromEvent(target, 'change')
     return listener.pipe(
-        scan(({ pulses }, { channel }) => {
-            return { channel, pulses: pulses ? pulses + 1 : 1 }
+        scan(({ pulses }, [channel, value]) => {
+            return { channel, value, pulses: pulses ? pulses + 1 : 1, counter: counter++ }
         })
     )
 }
 
+const init = () => {
+    return new Promise((resolve, reject) => {
+        gpio.setup(VALVE_CHANNEL, gpio.DIR_LOW, err => {
+            if (err) reject(err)
+            closeValve(gpio, () => console.warn('Close valve as workaround for initial state bug'))
+            gpio.setup(FLUX_CHANNEL, gpio.DIR_IN, gpio.EDGE_BOTH);
+            resolve(gpio)
+        });
+    })
+}
+
 module.exports = {
+    init: init,
     openValve: openValve,
     closeValve: closeValve,
     listenChannel: listenChannel,
