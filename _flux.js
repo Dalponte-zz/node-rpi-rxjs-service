6d925427cfe459898017c8b23400c69e27d72d4c
dalponte <maico.dalponte@codengage.com>
2020-05-12 19:21:47 -0300

    Change experiment files with more console information

--- _flux.js
+++ _flux.js
@@ -1,14 +1,72 @@
-var gpio = require('rpi-gpio');
+var gpio = require('rpi-gpio')
+var gpiop = require('rpi-gpio').promise
+const readline = require('readline')
 
-const ml_per_pulse = 0.214
-let pulse_count = 0, total_ml = 0
+const { fromEvent, interval } = require('rxjs')
+const { map, finalize, filter, throttle } = require('rxjs/operators')
 
-gpio.on('change', function(channel, value) {
+readline.emitKeypressEvents(process.stdin)
+process.stdin.setRawMode(true)
+const keypress = fromEvent(process.stdin, 'keypress')
+console.info(' \' - Setup \n 1 - Open \n 2 - Close \n CTRL+C - Terminar')
 
 
-		if(value) pulse_count++
-		
-		console.log({channel, pulse_count, value});	
-	
-});
-gpio.setup(13, gpio.DIR_IN, gpio.EDGE_BOTH);
+const listenPortChange = (port) => {
+	let count = 0
+	return fromEvent(gpio, 'change').pipe(
+		map(([channel, value], pulses) => {
+			console.log('CHANNEL: ', channel, 'PORT: ', port)
+			count++
+			const volume = Math.trunc(count * 0.146)
+			return { port, count, pulses, volume, value}
+		}),
+		throttle(val => interval(50))
+		// filter(({pulses}) => pulses % 8 === 0),
+	);
+}
+
+const FLOWMETER_CHANNEL = 15
+const TEST_CHANNEL = 11
+
+const init = async () => {
+	try {
+		keypress.subscribe(async ([event]) => {
+			await gpiop.setup(TEST_CHANNEL, gpio.DIR_LOW)
+
+			switch (event) {
+				case '\'':
+					await gpio.setup(FLOWMETER_CHANNEL, gpio.DIR_IN, gpio.EDGE_BOTH)
+					const flow = listenPortChange(FLOWMETER_CHANNEL)
+					flow.subscribe(payload => {
+						console.log('FLOW', payload)
+					})
+					break
+				case '1':
+					gpiop.write(TEST_CHANNEL, true)
+					break
+				case '2':
+					gpiop.write(TEST_CHANNEL, false)
+					break
+				case '3':
+					await gpio.setup(13, gpio.DIR_IN, gpio.EDGE_BOTH)
+					const t = listenPortChange(13)
+					t.subscribe(payload => {
+						console.log('TESTE', payload)
+					})
+					break
+				case '0':
+					gpio.destroy()
+					break
+				case '\u0003': // CTRL C termina aplicação
+					// CTRL + C to exit
+					gpio.destroy()
+					process.exit()
+			}
+		});
+	} catch (e) {
+		gpio.reset()
+		console.log(e)
+		process.exit()
+	}
+}
+init()
--- _valve.js
+++ _valve.js
@@ -1,44 +1,65 @@
 var gpio = require('rpi-gpio');
+const readline = require('readline')
+const { fromEvent } = require('rxjs')
 
-const readline = require('readline');
-readline.emitKeypressEvents(process.stdin);
+readline.emitKeypressEvents(process.stdin)
 process.stdin.setRawMode(true)
+const keypress = fromEvent(process.stdin, 'keypress')
 
+const VALVE_CHANNEL = 18
+const TEST1_CHANNEL = 13
 
-const open = () => {
-    gpio.write(11, false, function(err) {
-        if (err) throw err;
-        console.warn('[ ] The valve should be OPENED');
-    });
+console.info('1 - LOW \n2 -HIGH \nz - true \nx - false \nA ; S; D\nCTRL+C - Terminar')
+
+const cb = (err) => {
+    if (err) {
+        throw err
+    }
+    console.log(err)
 }
 
-const close = () => {
-    gpio.write(11, true, function(err) {
-        if (err) throw err;
-        console.warn('[X] The valve should be CLOSED');
-    });
+const init = async () => {
+    try {
+        keypress.subscribe(async ([event, key]) => {
+            switch (event) {
+                case '1':
+                    gpio.setup(VALVE_CHANNEL, gpio.DIR_LOW, cb)
+                    break
+                case '2':
+                    gpio.setup(VALVE_CHANNEL, gpio.DIR_HIGH, cb)
+                    break
+
+                case 'z':
+                    gpio.write(VALVE_CHANNEL, true, cb)
+                    break
+                case 'x':
+                    gpio.write(VALVE_CHANNEL, false, cb)
+                    break
+                
+                case 'a':
+                    gpio.setup(TEST1_CHANNEL, gpio.DIR_HIGH, cb)
+                    break
+                case 's':
+                    gpio.write(TEST1_CHANNEL, true, cb)
+                    break
+                case 'd':
+                    gpio.write(TEST1_CHANNEL, false, cb)
+                    break
+                    
+                
+                case 'q':
+                    gpio.destroy(cb)
+                    break
+                case '\u0003': // CTRL C termina aplicação
+                    // CTRL + C to exit
+                    gpio.destroy()
+                    process.exit()
+            }
+        });
+    } catch (e) {
+        gpio.destroy(cb)
+        console.log(e)
+        process.exit()
+    }
 }
-
-const init  = async () => {
-
-    gpio.setup(11, gpio.DIR_HIGH, open);
-
-    process.stdin.on('keypress', (str, {sequence}) => {
-        console.log('Key detected: ', sequence);
-
-        if(sequence === '\r'){
-            open()
-        }   else if(sequence === 'q') {
-            process.exit()
-        }   else {
-            close()
-        }
-    });
-
-}
-try{
-    init()
-}
-catch(err) {
-    process.exit()
-}
+init()
